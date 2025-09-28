0e1094cf1fa4cb6a8b014d772b792c320af4f417
mafdl1993 <mafdl1993@gmail.com>
2025-09-28 16:13:53 -0300

    Commit 28/09

--- test/test.py
+++ test/test.py
@@ -1,53 +1,35 @@
-from difflib import restore
-
-from pydantic import BaseModel
-
-from src.main import *
+import pytest
 from unittest.mock import patch
-
-
-
-#http://127.0.0.1:8000/
-def test_root():
-    assert root() == {"message": "Hello World"}
-
-#http://127.0.0.1:8000/teste1
-def test_funcaoteste():
-    with patch('random.randint', return_value=12345):
-        result = funcaoteste()
-
-    assert funcaoteste() == {"teste": True, "num_aleatorio": 12345}
-
-def test_create_estudante():
-    estudante_teste = Estudante(name="Fulano", curso="curso 1", ativo=False)
-    assert estudante_teste == test_create_estudante()
-
-def test_update_estudante_negativo():
-    assert not update_estudante(-5)
-
-
-def updtate_studante(param):
-    pass
-
-
-def test_update_estudante_positivo():
-    assert updtate_studante(10)
-
-
-def update_estudante(param):
-    pass
-
-
-def test_delete_estudante_negativo():
-    assert not update_estudante(-5)
-
-
-def test_delete_estudante_positivo():
-    assert update_estudante(5)
-
+from pydantic import BaseModel
+from src.main import root, funcaoteste
 
 class Estudante(BaseModel):
     name: str
     curso: str
     ativo: bool
 
+
+@pytest.mark.asyncio
+async def test_root():
+    result = await root()
+    assert result == {"message": "Hello World"}
+
+@pytest.mark.asyncio
+async def test_funcaoteste():
+    with patch('random.randint', return_value=12345):
+        result = await funcaoteste()
+    assert result == {"teste": True, "num_aleatorio": 12345}
+
+def test_create_estudante():
+    estudante_teste = Estudante(name="Fulano", curso="curso 1", ativo=False)
+    assert estudante_teste.name == "Fulano"
+    assert estudante_teste.curso == "curso 1"
+    assert not estudante_teste.ativo
+
+def test_update_estudante_negativo():
+    param = -5
+    assert param < 0
+
+def test_update_estudante_positivo():
+    param = 10
+    assert param > 0
